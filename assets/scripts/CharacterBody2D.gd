e3e198d59079a2f3200a843c5f69cac735b24419
Suchit Vemula <vemulasuchit3@gmail.com>
2023-11-18 11:55:30 -0500

    idek

--- assets/scripts/CharacterBody2D.gd
+++ assets/scripts/CharacterBody2D.gd
@@ -6,8 +6,9 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+var charge = 0
+func _ready():
+	pass
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -26,3 +27,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func add_coins():
+	charge += 1
+	print("coin collected")
